d4470b80c50d2ddaa8209285a21517ff5b5d9886
Vasilii <mihalcukv552@gmail.com>
2024-10-15 08:32:03 +1100

    1

---
+++ main.py
@@ -0,0 +1,69 @@
+import vectors as ve
+
+vector1 = [1, 2, 3]
+vector2 = [4, 5, 6]
+scalar = 3
+
+# Скалярное произведение
+dot_result = ve.myltiply_vectors(vector1, vector2)
+print(f"Скалярное произведение: {dot_result}")
+
+# Умножение вектора на скаляр
+scalar_result = ve.multiply_by_scalar(vector1, scalar)
+print(f"Результат умножения вектора на скаляр: {scalar_result}\n")
+
+
+
+vector1_2 = [1, 3.5, 3]
+vector2_2 = [4, 5, 6]
+scalar_2 = 3
+
+# Скалярное произведение
+dot_result = ve.myltiply_vectors(vector1_2, vector2_2)
+print(f"Скалярное произведение: {dot_result}")
+
+# Умножение вектора на скаляр
+scalar_result = ve.multiply_by_scalar(vector1_2, scalar_2)
+print(f"Результат умножения вектора на скаляр: {scalar_result}")
+
+
+
+vector1_3 = [1, "два", 3]
+vector2_3 = [4, 5, 6]
+scalar_3 = 3
+
+# Скалярное произведение
+dot_result = ve.myltiply_vectors(vector1_3, vector2_3)
+print(f"Скалярное произведение: {dot_result}")
+
+# Умножение вектора на скаляр
+scalar_result = ve.multiply_by_scalar(vector1_3, scalar_3)
+print(f"Результат умножения вектора на скаляр: {scalar_result}")
+
+
+
+vector1_4 = [1, 2, 3]
+vector2_4 = [4, 5, 6]
+scalar_4 = "три"
+
+# Скалярное произведение
+dot_result = ve.myltiply_vectors(vector1_4, vector2_4)
+print(f"Скалярное произведение: {dot_result}")
+
+# Умножение вектора на скаляр
+scalar_result = ve.multiply_by_scalar(vector1_4, scalar_4)
+print(f"Результат умножения вектора на скаляр: {scalar_result}")
+
+
+
+vector1_5 = [1, 2, 3]
+vector2_5 = [4, 5, 6, 7]
+scalar_5 = 3
+
+# Скалярное произведение
+dot_result = ve.myltiply_vectors(vector1_5, vector2_5)
+print(f"Скалярное произведение: {dot_result}")
+
+# Умножение вектора на скаляр
+scalar_result = ve.multiply_by_scalar(vector1_5, scalar_5)
+print(f"Результат умножения вектора на скаляр: {scalar_result}")
---
+++ vectors.py
@@ -0,0 +1,34 @@
+# vector_operations.py
+
+def myltiply_vectors(vector1, vector2):
+    if len(vector1) != len(vector2):
+        raise ValueError("Векторы должны быть одной длины.")
+
+    # Проверка, что все элементы векторов - числа
+    for v in vector1 + vector2:     # Объединяем два списка в один
+        if not isinstance(v, (int, float)):     # Проверяем, является ли тип элемента int или float
+            raise ValueError("Все элементы векторов должны быть числами.")
+
+    result = 0
+    for i in range(len(vector1)):
+        result += vector1[i] * vector2[i]
+
+    return result
+
+
+def multiply_by_scalar(vector, scalar):
+
+    # Проверка, что скаляр - число
+    if not isinstance(scalar, (int, float)):
+        raise ValueError("Скаляр должен быть числом.")
+
+    # Проверка, что все элементы вектора - числа
+    for v in vector:
+        if not isinstance(v, (int, float)):
+            raise ValueError("Все элементы вектора должны быть числами.")
+
+    result = []
+    for i in range(len(vector)):
+        result.append(vector[i] * scalar)
+
+    return result
